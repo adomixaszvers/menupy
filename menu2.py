2db02cd28ca3bb420fef8c22761ee88c5bf06a4b
Adomas Jatužis <adomixaszvers@gmail.com>
2013-04-27 11:40:37 +0300

    Initial commit

---
+++ menu2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+from PyQt4 import QtGui, QtCore
+import sys, os
+import keybinder
+import threading
+
+
+class Tray(QtGui.QSystemTrayIcon):
+  def __init__(self, variantai, parent=None):
+    super(QtGui.QSystemTrayIcon, self).__init__(QtGui.QApplication.style().standardIcon(QtGui.QStyle.SP_DriveDVDIcon), parent)
+    menu = QtGui.QMenu()
+    for variantas in variantai:
+      menu.addAction(QtGui.QAction(variantas, menu))
+
+    exitAction = QtGui.QAction('Exit', menu)
+    exitAction.setStatusTip('Exit application')
+    exitAction.triggered.connect(sys.exit)
+
+    menu.addAction(exitAction)
+    menu.triggered.connect(self.pakeisk)
+    
+    self.setContextMenu(menu)
+
+  def pakeisk(self, veiksmas):
+    variantas = veiksmas.text()
+    if variantas != "Exit":
+      os.popen("setxkbmap %s" % variantas)
+      self.showMessage("Pakeista", variantas, msecs=2000)
+      self.setToolTip(variantas)
+
+class KeyBinder:
+  def __init__(self, variantai, keystr, tray):
+    self.variantai = variantai
+    self.skaicius = 0
+    keybinder.bind(keystr, self.pakeisk)
+  def pakeisk(self):
+    self.skaicius += 1
+    if self.skaicius >= len(self.variantai):
+      self.skaicius = 0
+    variantas = self.variantai[self.skaicius]
+    os.popen("setxkbmap %s" % variantas)
+    tray.showMessage("Pakeista", variantas+"\n\n\n", msecs=2000)
+    tray.setToolTip(variantas)
+
+
+if __name__ == "__main__":
+  variantai = ["lt", "us"]
+  app = QtGui.QApplication(sys.argv)
+  tray = Tray(variantai)
+  tray.show()
+  key = KeyBinder(variantai, "<Shift>space", tray)
+  sys.exit(app.exec_())
